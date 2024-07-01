31535f4fcbad51eccc750a2ef40186c36eb25083
gedchess <gedchess@gmail.com>
2024-07-01 22:03:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstname ="Gediminas"
+let secondname ="Sarakauskas"
+console.log( firstname + secondname )
+let number = 15
+function Gedfunction(n) { for(i=1;i<=n;i++){
+    if(i % 3 == 0 && i % 5 == 0) {console.log ('Fizzbuzz')}
+    else if(i % 3 == 0) {console.log ("Fizz")} 
+    else if(i % 5 == 0) {console.log ('buzz')}
+    else {console.log(i);}
+} 
+}
+
+Gedfunction (number)
